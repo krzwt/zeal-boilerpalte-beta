ab098a68308f42ef7e00924d16907ff346108bb4
Gaurav <gaurav@zealousweb.com>
2025-03-31 17:01:38 +0530

    Removed Emojis scripts

--- includes/theme-action.php
+++ includes/theme-action.php
@@ -1,10 +1,22 @@
 <?php
-/**
-* Load the theme actions.
-*/
 
-if ( !defined( 'ABSPATH' ) ) {
-	header( 'Status: 403 Forbidden' );
-	header( 'HTTP/1.1 403 Forbidden' );
+/**
+ * Load the theme actions.
+ */
+
+if (!defined('ABSPATH')) {
+	header('Status: 403 Forbidden');
+	header('HTTP/1.1 403 Forbidden');
 	exit;
 }
+
+/**
+ * Removes unnecessary emoji scripts
+ */
+function disable_wp_emojis()
+{
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+}
+add_action('init', 'disable_wp_emojis');
