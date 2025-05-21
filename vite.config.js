432a4fcaa02104e648c13ba498c0cdac3601c38b
Mehul Gediya <mehul@zealousweb.com>
2025-05-21 18:52:30 +0530

    add file for js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path, { resolve } from "path";
 import glob from "fast-glob";
 
@@ -17,11 +17,12 @@ const getModuleEntries = () => {
 	return entries;
 };
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+	const env = loadEnv(mode, process.cwd());
 	const isProduction = process.env.NODE_ENV === "production";
 
 	return {
-		base: "/",
+		base: `${env.VITE_DOMAIN}/wp-content/themes/${env.VITE_WP_THEME_NAME}/assets/`,
 		build: {
 			outDir: "assets/",
 			manifest: true,
