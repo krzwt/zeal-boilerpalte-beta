365ee10b33ac9a6d91d2b9d4b10837b70343a36d
Mehul Gediya <mehul@zealousweb.com>
2025-05-23 19:12:23 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 // Webpack config (ES6 Compatible) with full comments
 
-import path from 'path';
-import { fileURLToPath } from 'node:url';
-import TerserPlugin from 'terser-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-import { WebpackManifestPlugin } from 'webpack-manifest-plugin';
+import path from "path";
+import { fileURLToPath } from "node:url";
+import TerserPlugin from "terser-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import { WebpackManifestPlugin } from "webpack-manifest-plugin";
 // import { PurgeCSSPlugin } from 'purgecss-webpack-plugin';
-import { glob } from 'glob';
+import { glob } from "glob";
 
 // Get current file path and directory
 const filename = fileURLToPath(import.meta.url);
@@ -15,10 +15,10 @@ const dirname = path.dirname(filename);
 
 // Detect if we're in development mode
 /* eslint-disable no-undef */
-const isDev = process?.argv?.includes('--watch') || false;
+const isDev = process?.argv?.includes("--watch") || false;
 /* eslint-enable no-undef */
 
-const enableModules = true; // Toggle this to true or false
+const enableModules = false; // Toggle this to true or false
 const getModuleEntries = () => {
 	const files = glob.sync("sources/js/modules/**/*.js");
 	const entries = {};
@@ -33,122 +33,155 @@ const getModuleEntries = () => {
 	return entries;
 };
 
-const jsFilename = isDev ? 'js/[name].dev.js' : 'js/[name].[contenthash].js';
-const cssFilename = isDev ? 'css/[name].dev.css' : 'css/[name].[contenthash].css';
+const jsFilename = isDev ? "js/[name].dev.js" : "js/[name].[contenthash].js";
+const cssFilename = isDev
+	? "css/[name].dev.css"
+	: "css/[name].[contenthash].css";
 
 // Base plugins always applied
 const plugins = [
-    new MiniCssExtractPlugin({
-        filename: cssFilename,
-    }),
-    new WebpackManifestPlugin({
-        publicPath: '/',     // Ensure proper public path
-        basePath: '',        // No double prefix
-    }),
-    //  new PurgeCSSPlugin({
-    //     paths: glob(`${dirname}/**/*.php`),
-    //     safelist: [], // Remove all safelisting temporarily
-    //     defaultExtractor: (content) => content.match(/[\w-]+/g) || [],
-    //     verbose: true,
-    // }),
+	new MiniCssExtractPlugin({
+		filename: cssFilename,
+	}),
+	new WebpackManifestPlugin({
+		publicPath: "/", // Ensure proper public path
+		basePath: "", // No double prefix
+	}),
+	//  new PurgeCSSPlugin({
+	//     paths: glob(`${dirname}/**/*.php`),
+	//     safelist: [], // Remove all safelisting temporarily
+	//     defaultExtractor: (content) => content.match(/[\w-]+/g) || [],
+	//     verbose: true,
+	// }),
 ];
 
 // Export Webpack config object
 export default {
-    mode: isDev ? 'development' : 'production',
+	mode: isDev ? "development" : "production",
 
-    entry: {
-        main: ['./sources/js/script.js', './sources/scss/style.scss'],
-        ...(enableModules ? getModuleEntries() : {})
-    },
+	entry: {
+		main: ["./sources/js/script.js", "./sources/scss/style.scss"],
+		...(enableModules ? getModuleEntries() : {}),
+	},
 
-    output: {
-        path: path.resolve(dirname, 'assets'),
-        filename: jsFilename,
-        clean: true,
-        publicPath: '/',
-    },
+	output: {
+		path: path.resolve(dirname, "assets"),
+		filename: jsFilename,
+		clean: true,
+		publicPath: "/",
+	},
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                        esmodules: true,
-                                    },
-                                    bugfixes: true,
-                                    modules: false,
-                                },
-                            ],
-                        ],
-                        cacheDirectory: true,
-                    },
-                },
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|webp|avif)$/i,
-                type: 'asset/resources',
-                generator: {
-                    filename: 'images/[name][ext][query]',
-                },
-            },
-        ],
-    },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							[
+								"@babel/preset-env",
+								{
+									targets: {
+										esmodules: true,
+									},
+									bugfixes: true,
+									modules: false,
+								},
+							],
+						],
+						cacheDirectory: true,
+					},
+				},
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: "../",
+						},
+					},
+					{
+						loader: "css-loader",
+						options: {
+							importLoaders: 1,
+							url: true,
+							sourceMap: true,
+						},
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true, // Enable Sass source maps
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|webp|avif)$/i,
+				type: "asset/resource",
+				generator: {
+					filename: "images/[name][ext][query]",
+				},
+			},
+			// Optional: fonts
+			{
+				test: /\.(woff(2)?|ttf|eot)$/,
+				type: "asset/resource",
+				generator: {
+					filename: "fonts/[name][ext][query]",
+				},
+			},
+		],
+	},
 
-    resolve: {
-        extensions: ['.scss','.js'],
-        alias: {
-            '@scss': path.resolve(dirname, 'sources/scss'),
-            '@js': path.resolve(dirname, 'sources/js'),
-        },
-    },
+	resolve: {
+		extensions: [".scss", ".js"],
+		alias: {
+			"@scss": path.resolve(dirname, "sources/scss"),
+			"@js": path.resolve(dirname, "sources/js"),
+			"@img": path.resolve(dirname, "sources/images"),
+		},
+	},
 
-    plugins,
+	plugins,
 
-    optimization: {
-        minimize: !isDev,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    ecma: 2023,
-                    compress: {
-                        drop_console: !isDev,
-                    },
-                },
-            }),
-            new CssMinimizerPlugin(),
-        ],
-    },
+	optimization: {
+		minimize: !isDev,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					ecma: 2023,
+					compress: {
+						drop_console: !isDev,
+					},
+				},
+			}),
+			new CssMinimizerPlugin(),
+		],
+	},
 
-    target: ['web', 'es2023'],
-  
-    devtool: 'cheap-module-source-map',
+	target: ["web", "es2023"],
 
-    watch: isDev,
+	devtool: "cheap-module-source-map",
 
-    stats: {
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false,
-    },
+	watch: isDev,
 
-    performance: {
-        hints: isDev ? false : 'warning',
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
-    },
+	stats: {
+		colors: true,
+		modules: false,
+		children: false,
+        timings: true,
+		chunks: false,
+		chunkModules: false,
+	},
+
+	performance: {
+		hints: isDev ? false : "warning",
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000,
+	},
 };
